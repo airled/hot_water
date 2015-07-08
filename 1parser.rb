318bcc034fd1bb950cf36a031255f9b9b1ee5a77
airled <airled@mail.ru>
2015-07-08 23:20:49 +0300

    advance

--- 1parser.rb
+++ 1parser.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
-#require './models'
+require './models'
 
 #fetching HTML
 #html = Nokogiri::HTML(open('http://www.belta.by/ru/dose_menu/grafik_zkh'))
@@ -16,10 +16,23 @@ text = html.xpath('//div[@class="main_block"]').text.strip
 orgs = ['УП "Минсккоммунтеплосеть"','РУП "Минскэнерго"','филиал "Минские тепловые сети"']
 orgs.map { |org| text.gsub!(org,"") }
 
+#fetching names of the streets from 'strong'-tags
+streets = []
+html.xpath('//div[@class="main_block"]//strong').map do |tag|
+  tag.text.split(/,|;/).map do |street|
+    streets << street unless street.include?('В период')
+  end
+end
+
 file = File.open('temp.txt','w')
 
-#dividing the text into the blocks by date
+#preparing the text for dividing by groups
 text.gsub!(/[^\ 0-9.,А-Яа-я;\/\-()–№\"]/,"").gsub!(' у потребителей по улицам','%').strip
+# streets.map do |street|
+#   text.sub!(street,'!!')
+# end
+
+#dividing the text into the groups by date
 main = text.split(/В период/).drop(1)
 
 #collecting dates and address groups into arrays
@@ -30,10 +43,23 @@ main.map do |date_with_group|
   groups << date_with_group.split('%')[1]
 end
 
-html.xpath('//div[@class="main_block"]//strong').map do |tag|
-  tag.text.split(/,|;/).map do |street|
-    (file << street << "\n") unless street.include?('В период')
+separated_streets = []
+groups.map do |group|
+  array = []
+  streets.map do |street|
+    array << street if group.include?(street)
   end
+  separated_streets << array
+end
+
+# dates.zip(groups).map do |date,group|
+#   group.split(/!!/).map do |houses|
+#     Record.create(date: date, house: houses)
+#   end
+# end
+
+separated_streets.map do |blabla|
+  file << blabla << "\n"
 end
 
 file.close
