3cc99ab01b058a3d17095d895e1b5006d25040b4
airled <airled@mail.ru>
2015-07-15 17:05:57 +0300

    refactoring

--- parser.rb
+++ parser.rb
@@ -2,42 +2,49 @@ require 'nokogiri'
 require 'open-uri'
 require './models'
 
-file = File.open('temp.txt','w')
-
-#fetching HTML
-#html = Nokogiri::HTML(open('http://www.belta.by/ru/dose_menu/grafik_zkh'))
-html = Nokogiri::HTML(open('html.txt'))
-
-#removing some unwanted tags  
-html.xpath('//div[@class="social"]').map(&:remove)
-html.xpath('//h2').map(&:remove)
-# html.xpath('//div[@class="main_block"]//span').delete(html.xpath('//div[@class="main_block"]//span').last)
-
+#fetching HTML and removing some unwanted tags  
+def fetched_html(source)
+  html = Nokogiri::HTML(open(source))
+  html.xpath('//div[@class="social"]').map(&:remove)
+  html.xpath('//h2').map(&:remove)
+  # html.xpath('//div[@class="main_block"]//span').delete(html.xpath('//div[@class="main_block"]//span').last)
+  html
+end
+  
 #collecting all text from main 'div' and removing some non-address stuff
-raw_text = html.xpath('//div[@class="main_block"]').text.strip
-orgs = ['УП "Минсккоммунтеплосеть"','РУП "Минскэнерго"','филиал "Минские тепловые сети"']
-orgs.map { |org| raw_text.gsub!(org,"") }
-
+def fetched_text(html)
+  raw_text = html.xpath('//div[@class="main_block"]').text
+  ['УП "Минсккоммунтеплосеть"','РУП "Минскэнерго"','филиал "Минские тепловые сети"'].map { |org| raw_text.gsub!(org,"") }
+  raw_text.gsub(/[^\ 0-9.,А-Яа-я;\/\-()–№\"]/,"").strip
+end
+  
 #fetching names of the streets from 'strong'-tags
-streets = []
-html.xpath('//div[@class="main_block"]//strong').map do |tag|
-  tag.text.split(/[,;]/).map do |street|
-    streets << street unless (street.include?('В период') || (((street.size < 5) && !(street[0] =~ /[а-я]/).nil?)) || street.size == 1)
+def streets_from_strongs(html)
+  streets = []
+  html.xpath('//div[@class="main_block"]//strong').map do |tag|
+    tag.text.split(/[,;]/).map do |street|
+      streets << street unless (street.include?('В период') || (((street.size < 5) && !(street[0] =~ /[а-я]/).nil?)) || street.size == 1)
+    end
   end
+  streets
 end
 
-#preparing the text for dividing by date groups
-text = raw_text.gsub(/[^\ 0-9.,А-Яа-я;\/\-()–№\"]/,"").gsub(' у потребителей по улицам','%').strip
+file = File.open('temp.txt','w')
+
+# source = 'http://www.belta.by/ru/dose_menu/grafik_zkh'
+source = 'html.txt'
+html = fetched_html(source)
+streets = streets_from_strongs(html)
 
 #dividing the text into the groups by date
-main = text.split('В период').drop(1)
+main = fetched_text(html).split('В период').drop(1)
 
 #collecting dates and address groups into arrays
 dates = []
 groups = []
 main.map do |date_with_group|
-  dates << date_with_group.split('%')[0]
-  groups << date_with_group.split('%')[1]
+  dates << date_with_group.split(' у потребителей по улицам')[0]
+  groups << date_with_group.split(' у потребителей по улицам')[1]
 end
 
 #temporary code for removing some non-<strong> names
@@ -45,25 +52,25 @@ dates.shift
 groups.shift
 
 #separating streets-array by date and replacing names of the streets in address groups for further splitting
-separated_streets = []
+streets_blocks = []
 groups.map do |group|
-  included = []
+  matched_streets = []
   streets.map.with_index do |street,index|
     if group.include?(street)
-      included << street
+      matched_streets << street
       group.sub!(street,'!!!')
       streets[index] = '@'
     end
   end
-  separated_streets << included
+  streets_blocks << matched_streets
 end
 
 #splitting groups by name of the streets from <strong>'s
-groups.map! { |group| group.split('!!!').drop(1) }
+houses_blocks = groups.map { |group| group.gsub(';',',').split('!!!').drop(1) }
 
-dates.zip(separated_streets,groups).map do |date,streets,group|
+dates.zip(streets_blocks,houses_blocks).map do |date,streets_block,houses_block|
   file << date << "\n" << "\n"
-  streets.zip(group).map do |street,houses|
+  streets_block.zip(houses_block).map do |street,houses|
     case
     when houses.scan(/[0-9А-Яа-я]/).empty?
       houses = ''
@@ -79,11 +86,20 @@ dates.zip(separated_streets,groups).map do |date,streets,group|
   file << "\n"
 end
 
-file.close
+# file.close
 
-# a = "1,3,6,32,4,6,3-56,234,457,234,34-657"
-# a_expanded =[]
-# a.split(/,/).map do |x|
-#     if x
-#     start = x.split(/-/)[0]
-#     stop = x.split(/-/)[1]
+# a = "1,2,3,4-6,7,8,9-11"
+# expanded =[]
+# a.split(/,/).map.with_index do |x,index|
+#     if x =~ /[0-9]+-[0-9]+/
+#         start = x.split(/-/)[0]
+#         stop = x.split(/-/)[1]
+#         start.upto(stop) do |y|
+#             expanded << y
+#         end
+#     elsif expanded << x
+#     end
+# end
+
+# expanded.join(',')
+
