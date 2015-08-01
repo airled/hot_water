a27575f1e2fbe868c8f505104c3cfc3061a0de0e
airled <airled@mail.ru>
2015-08-01 16:45:22 +0300

    -

--- parser.rb
+++ parser.rb
@@ -31,7 +31,7 @@ end
 #fetching ranges (like '12-20') and extending it in a sequence of values (array)
 def extended(range)
   sequence = []
-  unless range.include?('k')
+  unless range.include?('к')
     start = range.split('-')[0].to_i
     stop = range.split('-')[1].to_i
     case
@@ -105,11 +105,11 @@ dates.zip(streets_blocks,houses_blocks).map do |date,streets_block,houses_block|
       if houses_part =~ /[0-9]+-[0-9]+/
         extended(houses_part).map do |house|
           file1 << street + ' ||| ' + house.strip << "\n"
-          Record.create(date: date, street: street, house: house.strip)
+          # Record.create(date: date, street: street, house: house.strip)
         end
       else 
         file1 << street + ' ||| ' + houses_part.strip << "\n"
-        Record.create(date: date, street: street, house: houses_part.strip)
+        # Record.create(date: date, street: street, house: houses_part.strip)
       end
     end
     file1 << "\n"
