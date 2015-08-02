8a52fff4b7aadf9b95a10e603d9b24456f028c7c
airled <airled@mail.ru>
2015-08-02 18:04:57 +0300

    -

--- tests/test_finder.rb
+++ tests/test_finder.rb
@@ -1,17 +1,16 @@
 require 'minitest/autorun'
 require_relative '../finder.rb'
 
-class TestFinder < Minitest::Unit::TestCase
+class TestFinder < Minitest::Test
 	
   def setup
     @finder = Finder.new
   end
 
   def test_that_date_find
-  	correct = {street: 'Гинтовта', house: '36'}
-  	incorrect = {street: 'Блабла', house: 'Мимими'}
-  	assert_equal 'с 6 июля', @finder.date_find(correct[:street],correct[:house])
-  	assert_equal 'No info', @finder.date_find(incorrect[:street],incorrect[:house])
+  	existing = {street: 'Гинтовта', house: '36'}
+  	nonexisting = {street: 'Блабла', house: 'Мимими'}
+  	assert_equal 'с 6 июля', @finder.date_find(existing[:street],existing[:house])
+  	assert_equal 'No info', @finder.date_find(nonexisting[:street],nonexisting[:house])
   end
-
 end
