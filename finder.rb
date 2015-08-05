17e17ef9fd2f7a1764864d65aa32af13b050de89
airled <airled@mail.ru>
2015-08-05 10:52:54 +0300

    Finder corrected (find streets with *)

--- finder.rb
+++ finder.rb
@@ -3,8 +3,13 @@ require_relative './models'
 class Finder
 
   def date_find(street,house)
-    result = Record[street: street, house: house]
-    result.nil? ? 'No info' : result.date
+    result = if Record[street: street].house == '*'
+              Record[street: street].date
+             elsif Record[street: street, house: house].nil?
+              'No info'
+             else 
+              Record[street: street, house: house].date
+             end
   end
 
 end
