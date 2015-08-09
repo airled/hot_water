3b801a0f78f19b14ea00235050145e9623a1253a
airled <airled@mail.ru>
2015-08-09 14:46:07 +0300

    -

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
-@dir = "/home/airled/mina_test/current"
-# @dir = "/home/air/hot_water/"
+# @dir = "/home/airled/mina_test/current"
+@dir = "/home/air/hot_water/"
 
 worker_processes 2
 working_directory @dir
@@ -9,7 +9,7 @@ timeout 30
 # listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
 listen 3000 
 
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "#{@dir}/tmp/pids/unicorn.pid"
 
 stderr_path "#{@dir}/log/unicorn.stderr.log"
 stdout_path "#{@dir}/log/unicorn.stdout.log"
