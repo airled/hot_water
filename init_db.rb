563e07ecfdd848625cb3e44e689d7ee52dc8bc5e
airled <airled@mail.ru>
2015-08-16 14:27:20 +0300

    -

--- init_db.rb
+++ init_db.rb
@@ -1,12 +1,7 @@
 require "yaml"
 require "sequel"
 require "mysql2"
-# require "logger"
-
 
 #connecting to a MySQL database
 # hash = YAML.load_file("./config/database.yml")
 DB = Sequel.connect(:adapter => 'mysql2', :host => 'localhost', :user => 'root', :database => 'hot_water')
-
-
-# DB.loggers << Logger.new($stdout)
