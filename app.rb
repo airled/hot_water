e6f6729f7c57e6e249c16a9eaa4265f346264964
airled <airled@mail.ru>
2015-09-05 14:14:15 +0300

    some validation

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require './lib/finder'
 require 'json'
 require 'haml'
 
-class MyApp < Sinatra::Base
+# class MyApp < Sinatra::Base
 
   get '/' do
     haml :index
@@ -12,14 +12,14 @@ class MyApp < Sinatra::Base
   end
 
   get '/date' do
-    if params.empty?
-      'No params' 
-    else
+    if (params.has_key?(:street)) && (params.has_key?(:house)) && (params[:street].match(/[^А-Яа-я0-9\.\ ]/).nil?) && (params[:house].match(/[^А-Яа-я0-9\.\ ]/).nil?)
       street = params[:street]
       house = params[:house]
       date = Finder.new.date_find(street,house)
       {date: date}.to_json
+    else
+      'Params error' 
     end
   end
 
-end #class
+# end #class
