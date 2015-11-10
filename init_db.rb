82d3dc6242462e2c6679761efde745a547a29b75
airled <airled@mail.ru>
2015-11-10 16:00:41 +0300

    update

--- init_db.rb
+++ init_db.rb
@@ -3,5 +3,9 @@ require "sequel"
 require "mysql2"
 
 #connecting to a MySQL database
-# hash = YAML.load_file("./config/database.yml")
-DB = Sequel.connect(:adapter => 'mysql2', :host => 'localhost', :user => 'root', :database => 'hotwater')
+DB = Sequel.connect(
+  adapter: 'mysql2',
+  host: 'localhost',
+  user: 'root',
+  database: 'hotwater'
+)
