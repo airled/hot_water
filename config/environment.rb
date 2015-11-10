35f30e9d8d2826b078faadf88ee95c64c96643cb
airled <airled@mail.ru>
2015-11-10 15:35:42 +0300

    ||=

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require 'yaml'
 class ENV_adder
 
   def self.add_from_yaml
-    rack_env = ENV['RACK_ENV']
+    rack_env = ENV['RACK_ENV'] ||= 'development'
     hash = YAML.load_file("./config/environments/#{rack_env}.yml")
     hash.each_pair { |pair| ENV["#{pair[0]}"] = pair[1] }
   end
