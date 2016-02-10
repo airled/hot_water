7f02acf14db83ad4294add9bb88115c1f96a76ee
airled <airled@mail.ru>
2016-02-10 18:00:26 +0300

    listen to localhost in production

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ worker_processes 2
 working_directory @dir
 timeout 30
 # listen "#{@dir}/tmp/sockets/unicorn.sock", :backlog => 64
-listen 3001
+listen '127.0.0.1:3001'
 pid "#{@dir}/tmp/pids/unicorn.pid"
 stderr_path "#{@dir}/log/unicorn.stderr.log"
 stdout_path "#{@dir}/log/unicorn.stdout.log"
