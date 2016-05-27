0ac1962aad03de02d1ce79b827dfa0711c435456
airled <airled@mail.ru>
2016-05-27 21:07:02 +0300

    Trim for values

--- app/assets/javascripts/search-form.js
+++ app/assets/javascripts/search-form.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
   $('#search-submit').click(function() {
-    var street = $('#street').val();
-    var house = $('#house').val();
-    var url_for_date = Routes.root_path() + 'date?street=' + street + '&house=' + house;
-    $.ajax({
+    var street = $('#street').val().trim();
+    var house = $('#house').val().trim();
+    if (street === ''){
+      $('#result').text('Нет улицы');
+    }
+    else if (house === '') {
+      $('#result').text('Нет номера дома');
+    }
+    else {
+      var url_for_date = Routes.root_path() + 'date?street=' + street + '&house=' + house;
+      $.ajax({
         url: url_for_date
       })
       .done(function(data) {
         $('#result').text('Адрес: ' + street + ', ' + house + " | Отключение: " + data.date);
       });
+    }
   });
 });
